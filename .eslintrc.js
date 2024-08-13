5003b6a804d0f88a87dc9ed97d93787c793d6455
A5rocks <git@helvetica.moe>
2024-08-13 13:00:08 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["clarity/typescript"],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  // rules: {
+  //   "@typescript-eslint/no-floating-promises": "off",
+  //   "no-await-in-loop": "off",
+  //   "deprecation/deprecation": "warn",
+  // },
+  // plugins: ["deprecation"],
+};
